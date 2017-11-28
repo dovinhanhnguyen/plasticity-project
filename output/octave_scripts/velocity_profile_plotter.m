212a131f1c821789d8efc487f26dd3995ea0574e
Anh Nguyen <dovinhanh.nguyen@yahoo.co.uk>
2017-11-28 10:25:13 +0000

    Bulk Region

--- output/octave_scripts/velocity_profile_plotter.m
+++ output/octave_scripts/velocity_profile_plotter.m
@@ -3,6 +3,7 @@ graphics_toolkit("gnuplot");
 
 YMAX = 0.016713;
 VELOCITY_LAYERS = 20;
+NUMBER_OF_CUTOFF_LAYERS = 2;
 MEAN_DIAMETER = 0.001;
 normalized_layer_height = (YMAX/VELOCITY_LAYERS)/MEAN_DIAMETER;
 
@@ -10,15 +11,15 @@ SHEAR_TIMESTEPS = 600000000;
 NEVERY = 10000;
 plotting_nevery = 10000000;
 
-plotted_data = zeros(VELOCITY_LAYERS,2);
+plotted_data = zeros(VELOCITY_LAYERS-2*NUMBER_OF_CUTOFF_LAYERS,2);
 
 velocity_profile_data = csvread("../raw_data/velocity_profile.csv");
 
 set(0, 'defaultfigurevisible', 'off');
 figure;
 for j = 1:(plotting_nevery/NEVERY):(SHEAR_TIMESTEPS/NEVERY+1)
-	for i = 1:VELOCITY_LAYERS
-		plotted_data(i,1) = (i-0.5)*normalized_layer_height; % first column stores center of layers
+	for i = 1:(VELOCITY_LAYERS-2*NUMBER_OF_CUTOFF_LAYERS)
+		plotted_data(i,1) = (i+NUMBER_OF_CUTOFF_LAYERS-0.5)*normalized_layer_height; % first column stores center of layers
 		plotted_data(i,2) = mean(velocity_profile_data(1:j,(i+1))); % second column stores mean velocity, normalized by mean atom diameter
 	endfor
 	
